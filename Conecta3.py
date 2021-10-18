9b6180b8e22585c7fa3a35185cd4e8a284ca4825
Carlos Contreras <cdca12mx@gmail.com>
2021-10-17 18:26:48 -0600

    Implementación de algoritmo MiniMax para elegir el mejor movimiento de la PC

--- Conecta3.py
+++ Conecta3.py
@@ -5,11 +5,10 @@ import sys
 import math
 
 # Configuracion de juego
-FILAS = 6
-COLUMNAS = 7
-#FILAS = 3
-#COLUMNAS = 3
+FILAS = 3
+COLUMNAS = 3
 CONECTA_FICHAS = 3
+DIFICULTAD = 3
 
 JUGADOR = 0
 PC = 1
@@ -111,13 +110,13 @@ def mostrarGanador(ficha):
     # Volver a pintar rectangulo negro antes de mostrar mensaje
     pygame.draw.rect(pantalla, NEGRO, (0, 0, ANCHO_TABLERO, TAMANO_CUADRADO)) 
     if ficha == FICHA_JUGADOR: 
-        font = pygame.font.SysFont("monospace", 50)
+        font = pygame.font.SysFont("monospace", 25)
         label = font.render("¡Gana el Jugador!", 1, COLOR_JUGADOR)
-        pantalla.blit(label, (100, 25))
+        pantalla.blit(label, (25, 30))
     elif ficha == FICHA_PC:
-        font = pygame.font.SysFont("monospace", 50)
+        font = pygame.font.SysFont("monospace", 40)
         label = font.render("Gana la PC", 1, COLOR_PC)
-        pantalla.blit(label, (195, 25))
+        pantalla.blit(label, (30, 25))
 
 # Inteligencia Artificial
 
@@ -132,20 +131,20 @@ def evaluarVentana(ventana, ficha):
         puntuacion += 100
     # Conecta 2
     elif ventana.count(ficha) == (CONECTA_FICHAS - 1) and ventana.count(FICHA_VACIA) == 1:
-        puntuacion += 10
+        puntuacion += 5
     # Fichas contrincante
     if ventana.count(fichaContraria) == (CONECTA_FICHAS - 1) and ventana.count(FICHA_VACIA) == 1:
-        puntuacion -= 80
+        puntuacion -= 4
 
     return puntuacion
 
-def calcularPuntuacionDePosicion(tablero, ficha):
+def calcularPuntuacionTablero(tablero, ficha):
     puntuacion = 0
 
     # Puntuacion columna central
     arregloCentral = [int(i) for i in list(tablero[:, COLUMNAS // 2])]
     contadorCentral = arregloCentral.count(ficha)
-    puntuacion += contadorCentral * 6
+    puntuacion += contadorCentral * 3
 
     # Puntuacion horizontal
     for fila in range(FILAS):
@@ -174,25 +173,79 @@ def calcularPuntuacionDePosicion(tablero, ficha):
 
     return puntuacion
 
-def obtenerPosicionValidas(tablero):
+# Implementación de Algoritmo Mini-Max
+
+## Nodo: En este caso, sería el tablero
+## Nivel: Qué tan lejos vamos a buscar hacia abajo en nuestro juego
+## maximizandoJugador: Será True para la PC y Falso cuando busquemos los movimientos del Jugador
+## NodoTerminal: Es cuando un juego se gana (Jugador o PC) o se llega al final del juego
+## - Aquí se regresará el valor heurístico de ese nodo, si no, revisamos recursivamente el arbol para encontrar la mejor puntuación
+
+def esNodoTerminal(nodo):
+    return movimientoGanador(nodo, FICHA_JUGADOR) or movimientoGanador(nodo, FICHA_PC) or len(obtenerPosicionesValidas(nodo)) == 0
+
+def miniMax(nodo, nivel, maximizandoJugador):
+    nodoTerminal = esNodoTerminal(nodo)
+    tablero = nodo # Solo para darle más sentido semántico al código
+    posicionesValidas = obtenerPosicionesValidas(tablero)
+    # Caso base, obtenemos valor heurístico del nodo (tablero)
+    if nivel == 0 or nodoTerminal:
+        if nodoTerminal:
+            if movimientoGanador(tablero, FICHA_PC):
+                return (None, 10000000000000000) # Regresamos la mejor puntuación 
+            elif movimientoGanador(tablero, FICHA_JUGADOR):
+                return (None, -10000000000000000) # Regresamos la peor puntuación
+            else: # Game Over, sin más movimientos válidos
+                return (None, 0) 
+        else: # nivel es 0
+            return (None, calcularPuntuacionTablero(tablero, FICHA_PC))
+    if maximizandoJugador: # Entra cuando es la PC
+        valorActual = -math.inf
+        mejorColumna = random.choice(posicionesValidas)
+        for columna in posicionesValidas:
+            fila = obtenerSiguienteFila(tablero, columna)
+            tableroTemporal = tablero.copy()
+            ponerFicha(tableroTemporal, fila, columna, FICHA_PC)
+            nuevaPuntuacion = miniMax(tableroTemporal, nivel - 1, False)[1]
+            # Obtener el maximo y guardar columna
+            if nuevaPuntuacion > valorActual:
+                valorActual = nuevaPuntuacion
+                mejorColumna = columna
+        return (mejorColumna, nuevaPuntuacion)
+    else: # Minimizando jugador, entra cuando es el Jugador
+        valorActual = math.inf
+        mejorColumna = random.choice(posicionesValidas)
+        for columna in posicionesValidas:
+            fila = obtenerSiguienteFila(tablero, columna)
+            tableroTemporal = tablero.copy()
+            ponerFicha(tableroTemporal, fila, columna, FICHA_JUGADOR)
+            nuevaPuntuacion = miniMax(tableroTemporal, nivel - 1, True)[1]
+            # Obtener el minimo y guardar columna
+            if nuevaPuntuacion < valorActual:
+                valorActual = nuevaPuntuacion
+                mejorColumna = columna
+        return (mejorColumna, nuevaPuntuacion)
+
+
+def obtenerPosicionesValidas(tablero):
     posicionesValidas = []
     for columna in range(COLUMNAS):
         if esPosicionValida(tablero, columna):
             posicionesValidas.append(columna)
     return posicionesValidas
 
-
+# Inteligencia anterior, ya no se usa
 def elegirMejorMovimiento(tablero, ficha):
-    posicionesValidas = obtenerPosicionValidas(tablero)
+    posicionesValidas = obtenerPosicionesValidas(tablero)
     mejorPuntuacion = -10000
     mejorColumna = random.choice(posicionesValidas)
     for columna in posicionesValidas:
         fila = obtenerSiguienteFila(tablero, ficha)
         tableroTemporal = tablero.copy()
         ponerFicha(tableroTemporal, fila, columna, ficha)
-        puntuacion = calcularPuntuacionDePosicion(tableroTemporal, ficha)
+        puntuacion = calcularPuntuacionTablero(tableroTemporal, ficha)
 
-        # Comparar puntuaciones
+        # Comparar puntuaciones, simple método de búsqueda secuencial
         if puntuacion > mejorPuntuacion: 
             mejorPuntuacion = puntuacion
             mejorColumna = columna
@@ -242,8 +295,9 @@ def jugarJuego():
 
         # Turno de la PC
         if turno == PC and not gameOver:
-            # columna = random.randint(0, COLUMNAS - 1)
-            columna = elegirMejorMovimiento(tablero, FICHA_PC)
+            # columna = random.randint(0, COLUMNAS - 1) # Columna random, 0 logica
+            # columna = elegirMejorMovimiento(tablero, FICHA_PC) # Inteligencia basica anterior
+            columna, valorMiniMax = miniMax(tablero, DIFICULTAD, True) # True porque es turno de la PC
             
             if esPosicionValida(tablero, columna):
                 pygame.time.wait(500)
